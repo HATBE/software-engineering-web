322896abf299fd3bd1c53d2172b808ce41a156fa
HATBE2113 <admin@aarongensetter.ch>
2024-09-07 10:13:13 +0200

    todo list

---
+++ script.js
@@ -0,0 +1,58 @@
+const todoSubmitEl = document.getElementById("submit");
+const nameEl = document.getElementById("name");
+const todoTextEl = document.getElementById("todoText");
+const todoListEl = document.getElementById("todoList");
+
+let id = 0;
+
+let todos = [];
+
+todoSubmitEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const name = nameEl.value;
+  const todoText = todoTextEl.value;
+
+  if (name == "" || todoText == "") {
+    return;
+  }
+
+  const todo = {
+    id,
+    name,
+    todoText,
+  };
+
+  id++;
+
+  todos.push(todo);
+
+  nameEl.value = "";
+  todoTextEl.value = "";
+
+  renderTodos();
+});
+
+function renderTodos() {
+  todoListEl.innerHTML = "";
+  todos.forEach((todo) => {
+    const newTodoEl = document.createElement("li");
+    const newTodoTextEl = document.createElement("p");
+    newTodoTextEl.textContent = `${todo.name}: ${todo.todoText}`;
+
+    newTodoEl.appendChild(newTodoTextEl);
+
+    const xBtn = document.createElement("button");
+    xBtn.id = todo.id;
+
+    xBtn.addEventListener("click", () => {
+      // Convert xBtn.id to a number for comparison
+      todos = todos.filter((obj) => obj.id !== Number(xBtn.id));
+      renderTodos(); // Re-render the todo list after deletion
+    });
+    xBtn.textContent = "X";
+    newTodoEl.appendChild(xBtn);
+
+    todoListEl.appendChild(newTodoEl);
+  });
+}
